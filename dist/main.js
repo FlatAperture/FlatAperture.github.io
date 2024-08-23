a3f135a52a2122440a59a39c2f3413c34be88e40
Portal-05 <chiel36701@gmail.com>
2024-08-23 15:09:25 +0200

    compile

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Main Content Loaded");
+console.log("Main Typescript Entrypoint Found");
 //# sourceMappingURL=main.js.map
